67c454569cf1842a2fdfb8e82abc02f0f7033a0a
enzozafra <zafra@ualberta.ca>
2018-03-27 17:55:33 -0600

    finished tdee

--- Assignments/TMA1/part4/js/script.js
+++ Assignments/TMA1/part4/js/script.js
@@ -4,9 +4,11 @@ $(document).ready(function() {
 
 var requiredinfo = {};
 var tabclicked = "";
+var args = {};
 
 function loadrequired(tab) {
   requiredinfo = {};
+  args = {};
   $($('form').prop('elements')).each(function(){
     var name = this["name"];
     if (name != '') {
@@ -24,18 +26,98 @@ function loadrequired(tab) {
   }
 }
 
+function verifyInput() {
+  if (tabclicked == "unitconverter") {
+    // TODO:
+  }
+  else if (tabclicked == "mortgage") {
+    // TODO
+  }
+  else if (tabclicked == "tdee") {
+    // TODO
+    args['age'] = parseInt(args['age']);
+    args['weight'] = parseInt(args['weight']);
+
+    if (isNaN(args['age']) || isNaN(args['weight']) || !args['gender'] || !args['height'] || !args['activity']) {
+      return false;
+    }
+    return true;
+  }
+}
+
+function convertHeight(height) {
+  // var pattern = /(.*)ft(.*)in/;
+  var pattern = new RegExp('(.*)ft(.*)in');
+  var match = pattern.exec(height);
+  var ft = parseInt(match[0]);
+  var inch = parseInt(match[1]);
+
+  return (ft * 30.48) + (inch * 2.54);
+}
+
+function convertWeight(weight) {
+  return (weight * 0.453592);
+}
+
 function calculate() {
   console.log("calculating " + tabclicked);
+  if (tabclicked == "unitconverter") {
+    // TODO:
+  }
+  else if (tabclicked == "mortgage") {
+    // TODO
+  }
+  else if (tabclicked == "tdee") {
+    calculateTDEE();
+  }
+}
+
+function calculateTDEE() {
+  var gender = args['gender'];
+  var age = args['age'];
+  var weight = args['weight'];
+  var height = args['height'];
+  var activity = args['activity'];
+
+  var weight = convertWeight(weight);
+  var height = convertHeight(height);document
+
+  if (gender == "f") {
+    var bmr = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age);
+  }
+  else if (gender == "m") {
+    var bmr = 66 + (13.7 * weight) + (5 * height) - (6.8 * age);
+  }
+
+  var tdee;
+  if (activity == "s") {
+    tdee = bmr * 1.2;
+  }
+  else if (activity == "le") {
+    tdee = bmr * 1.375;
+  }
+  else if (activity == "me") {
+    tdee = bmr * 1.55;
+  }
+  else if (activity == "he") {
+    tdee = bmr * 1.725;
+  }
+  else if (activity == "a") {
+    tdee = bmr * 1.9;
+  }
+  var result = document.getElementById("result");
+  result.innerHTML = Math.round(tdee).toString() + " per day";
 }
 
 function checkCalc(name) {
   var element = document.getElementsByName(name);
   if (element && element[0].value ){
     requiredinfo[name] = true;
+    args[name] = element[0].value;
   } else {
     requiredinfo[name] = false;
+    args[name] = "";
   }
-  console.log(requiredinfo);
   checkAllFilled();
 }
 
@@ -43,9 +125,11 @@ function checkAllFilled() {
   var keys = Object.keys(requiredinfo);
   for (var i = 0; i < keys.length; i++) {
     if (!requiredinfo[keys[i]]) {
-      console.log(keys[i]);
       return;
     }
+  }
+  if(!verifyInput()) {
+    return;
   }
   calculate();
 }
@@ -75,44 +159,50 @@ $('ul.tabs').on('click', 'a', function(e) {
 
 // Set event listeners
 function listenTDEE() {
+  args['height'] = $("#height").val();
   $("#height").on('change', function() {
-    console.log($(this).val());
+    args['height'] = $(this).val();
     checkAllFilled();
   });
 
+  args['activity'] = $("#activity").val();
   $("#activity").on('change', function() {
-    console.log($(this).val());
+    args['activity'] = $(this).val();
     checkAllFilled();
   });
 
   $('.gender label').click(function() {
     var name = $(this).prev().attr('name');
     requiredinfo[name] = true;
-    console.log($(this).attr('for'));
+    args[name] = $(this).attr('for');
     checkAllFilled();
   });
 }
 
 function listenMort() {
+  args['freq'] = $("#freq").val();
   $("#freq").on('change', function() {
-    console.log($(this).val());
+    args['freq'] = $(this).val();
     checkAllFilled();
   });
 }
 
 function listenUnit() {
+  args['measuretype'] = $("#measuretype").val();
   $("#measuretype").on('change', function() {
-    console.log($(this).val());
+    args['measuretype'] = $(this).val();
     checkAllFilled();
   });
 
+  args['convunit'] = $("#convunit").val();
   $("#convunit").on('change', function() {
-    console.log($(this).val());
+    args['convunit'] = $(this).val();
     checkAllFilled();
   });
 
+  args['origunit'] = $("#origunit").val();
   $("#origunit").on('change', function() {
-    console.log($(this).val());
+    args['origunit'] = $(this).val();
     checkAllFilled();
   });
 }
